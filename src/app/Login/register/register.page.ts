7537a8e3882f94987747f513ec0d47dbf762f352
Agustin <agustin.caceres1996@gmail.com>
2019-05-26 19:05:46 -0300

    Fixes varios

--- src/app/Login/register/register.page.ts
+++ src/app/Login/register/register.page.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { RestService } from '../../Services/rest.service';
-import { DTnewuser } from '../../models/models';
-import { NavController } from '@ionic/angular';
-import { Storage } from '@ionic/storage';
 import { DTUser } from 'src/app/models/user/dtuser';
 import { UserService } from 'src/app/Services/user/user.service';
 import { AuthService } from 'src/app/Services/auth/auth.service';
+import { ToastService } from 'src/app/Services/toast/toast.service';
 
 
 @Component({
@@ -13,26 +10,44 @@ import { AuthService } from 'src/app/Services/auth/auth.service';
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage {
+export class RegisterPage implements OnInit {
 
-  user : DTUser = new DTUser();
+  user: DTUser = new DTUser();
 
   constructor(
-    public rest: RestService,
-    public navctrl: NavController,
-    public storage: Storage,
-    private userAPI : UserService,
-    private auth : AuthService
+    private userAPI: UserService,
+    private auth: AuthService,
+    private toast: ToastService
   ) { }
 
-  public register(){
+  ngOnInit() { }
+
+  public register() {
+    this.toast.presentToast("Cargando...", 'primary');
     this.userAPI.register(this.user).subscribe(
       data => {
         console.log(data);
-        let u : DTUser = data;
-        this.auth.login(u,2);
+        let u: DTUser = data;
+        this.toast.dismiss();
+        this.auth.login(u, 2);
+      },
+      error => {
+        console.log(error);
+        if (!error.error) {
+          this.toast.presentToast("El correo ingresado ya esta siendo utilizado.", 'danger');
+        } else {
+          this.toast.presentToast("Algo salio mal. Intentelo de nuevo.", 'danger');
+        }
       }
     );
+  }
+
+  isValid() {
+    return (
+      this.user.nombre.length > 0 &&
+      this.user.apellido.length > 0 &&
+      this.user.email.length > 0
+    )
   }
 
 }
--- src/app/miperfil/misdatos/misdatos.page.ts
+++ src/app/miperfil/misdatos/misdatos.page.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DTUser } from 'src/app/models/user/dtuser';
 import { Storage } from '@ionic/storage';
 import { UserService } from 'src/app/Services/user/user.service';
-import { ToastController, NavController } from '@ionic/angular';
-import { AuthService } from 'src/app/Services/auth/auth.service';
+import { NavController } from '@ionic/angular';
+import { ToastService } from 'src/app/Services/toast/toast.service';
 
 @Component({
   selector: 'app-misdatos',
@@ -12,14 +12,15 @@ import { AuthService } from 'src/app/Services/auth/auth.service';
 })
 export class MisdatosPage implements OnInit {
 
-    user : DTUser = new DTUser();
+  user: DTUser = new DTUser();
+
+  user_origin: DTUser = new DTUser();
 
   constructor(
-    private storage : Storage,
-    private userAPI : UserService,
-    private auth : AuthService,
-    private toastController : ToastController,
-    private navCtrl : NavController
+    private storage: Storage,
+    private userAPI: UserService,
+    private navCtrl: NavController,
+    private toast: ToastService
   ) { }
 
   ngOnInit() {
@@ -27,32 +28,37 @@ export class MisdatosPage implements OnInit {
       response => {
         console.log(response);
         this.user = response;
-      }
-    );    
-  }  
-
-  update(){
-    this.userAPI.update(this.user).subscribe(
-      result =>{
-        console.log(result);
-        this.storage.set('me',result);
-        this.presentToast('Los datos han sido actualizados.','success');
-        location.reload();
-        this.navCtrl.navigateBack('/tabs/tab1');
-      }, error => {
-        this.presentToast('No se han podido actualizar los datos.','danger');
-        //mostrar toast
+        this.user_origin = response;
       }
     );
   }
 
-  async presentToast(msg, type) {
-    const toast = await this.toastController.create({
-      message: msg,
-      duration: 2000,
-      color: type
-    });
-    toast.present();
+  update() {
+    if (this.hasChanged()) {
+      this.toast.presentToast("Cargando...", "primary");
+      this.userAPI.update(this.user).subscribe(
+        result => {
+          console.log(result);
+          this.storage.set('me', result);
+          this.toast.presentToast('Los datos han sido actualizados.', 'success');
+          location.reload();
+          this.navCtrl.navigateBack('/tabs/tab1');
+        },
+        error => {
+          console.log(error);
+          this.toast.presentToast('No se han podido actualizar los datos.', 'danger');
+        }
+      );
+    } else {
+      this.toast.presentToast("No se ha editado ningun dato.","warning");
+    }
   }
 
+  hasChanged() {
+    return !(
+      this.user.nombre == this.user_origin.nombre &&
+      this.user.apellido == this.user_origin.apellido &&
+      this.user.email == this.user_origin.email
+    )
+  }
 }
